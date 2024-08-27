c1bff0f5a62ea664f34f16ccbe1c59698a8b809f
Aman Bhardwaj <amanjeetbhardwaj3@gmail.com>
2024-08-27 18:16:01 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (30.58%) - LeetSync

---
+++ 27-remove-element/remove-element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        auto it=nums.begin();
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==val){
+                nums.erase(it+i);
+                i--;
+                it=nums.begin();
+            }
+        }
+        return nums.size();
+    }
+};
